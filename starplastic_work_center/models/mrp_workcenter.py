7ca7b9773cf02ef6afa63cd700df0d28aae1fcbd
Kuldeep Singh <kuldeep.singh@softbay.com>
2025-09-26 11:59:12 +0530

    Update mrp_workcenter.py
    Updated the List view of the work center

--- starplastic_work_center/models/mrp_workcenter.py
+++ starplastic_work_center/models/mrp_workcenter.py
@@ -4,11 +4,10 @@ class MrpWorkcenter(models.Model):
     _inherit = 'mrp.workcenter'
 
     machine_no = fields.Char(string="Machine No")
+    name = fields.Char(string="Machine Name")
     machine_type = fields.Selection([
         ('blow_molding', 'Blow Molding'),
         ('injection', 'Injection Molding'),
-        ('cnc', 'CNC'),
-        ('lathe', 'Lathe'),
         ('other', 'Other'),
     ], string="Machine Type")
 
@@ -17,8 +16,8 @@ class MrpWorkcenter(models.Model):
     remark = fields.Text(string="Remark")
     max_power = fields.Float(string="Max Power Consumption (Unit/hour)")
     outgoing_job = fields.Boolean(string="Outgoing Job?")
+    company_name = fields.Char(string="Company Name")
     machine_shift = fields.Selection([
-        ('day', 'Day'),
-        ('night', 'Night'),
+        ('day', 'Day Night'),
         ('3shift', '3 Shift'),
     ], string="Machine Shift")
